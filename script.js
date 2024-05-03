bcfe26edb2940e37a069ed48386e74bcd9259f7a
Glitch (boatneck-diagnostic-client) <none>
2024-05-03 06:54:06 +0000

    Glitch - Actualización PtoInfo

--- script.js
+++ script.js
@@ -1,34 +1,13 @@
-/*
-  This is your site JavaScript code - you can add interactivity!
-*/
+AFRAME.registerComponent('info-panel', { // Se genera un nuevo componente para la interfaz
+  init: function () { // Se le otorgan funciones al componente
+    this.infoButton = this.el.children[0]; // El primer hijo del padre que contenga info-panel se designa como infoButton (botón de información)
+    this.infoPanel = this.el.children[1]; // El segundo hijo del padre que contenga info-panel se designa como infoPanel (panel de información)
+    this.onInfoButtonClick = this.onInfoButtonClick.bind(this);
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
+    this.infoButton.addEventListener('click', this.onInfoButtonClick) // Detección de click 
+  },
 
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the steps in the TODO 🚧
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-if (btn) { // Detect clicks on the button
-  btn.onclick = function () {
-    // The 'dipped' class in style.css changes the appearance on click
-    btn.classList.toggle("dipped");
-  };
-}
-
-
-// ----- GLITCH STARTER PROJECT HELPER CODE -----
-
-// Open file when the link in the preview is clicked
-let goto = (file, line) => {
-  window.parent.postMessage(
-    { type: "glitch/go-to-line", payload: { filePath: file, line: line } }, "*"
-  );
-};
-// Get the file opening button from its class name
-const filer = document.querySelectorAll(".fileopener");
-filer.forEach((f) => {
-  f.onclick = () => { goto(f.dataset.file, f.dataset.line); };
+  onInfoButtonClick: function () {
+    this.infoPanel.object3D.visible = !this.infoPanel.object3D.visible; // Invierte el estado, si está en false (invisible), al hacer click se vuelve true (visible)
+  }
 });
